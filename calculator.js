5b9c988e976b9966645fc7eb82164660737857e5
mahabubimon <mahabubemon24@gmail.com>
2022-01-10 22:52:19 +0600

    test

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+window.onbeforeunload = function () {
+  return "Are you sure you want to leave?";
+};
+
 class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement;
